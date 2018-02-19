f3186ff3ae87931849f0745415c5a396e276bd25
Rob Weber <rob.weber87@gmail.com>
2018-02-19 17:30:29 -0500

    added a few more projectcts corrected on hover placement

--- src/ListProjects.js
+++ src/ListProjects.js
@@ -5,8 +5,10 @@ import Project from './Project.js'
 import {Card, CardMedia, CardTitle} from 'material-ui/Card';
 import img1 from './images/Haufe_TaschenguideAktion_1.jpg';
 import img2 from './images/Book3.jpg';
-import img3 from './images/Cover_Landingpage.jpg';
 import img4 from './images/Kloepfer_Sortimentsbroschuere_Titel.jpg';
+import img5 from './images/Theresa_KompositionFINAL.jpg';
+import img6 from './images/Fitz_landingpage.jpg';
+import img7 from './images/BayLGB_Landingpage.jpg';
 
 
 
@@ -18,24 +20,34 @@ class ListProjects extends Component {
 		this.state = {
 			projects: [
 				{
-					title: "Aus der Asche",
+					title: "print",
 					subtitle: "Book by Wolfgang Bilinski",
 					img: img1
 				},
 				{
-					title: "Aus der Asche",
-					subtitle: "Book by Wolfgang Bilinski",
+					title: "print",
+					subtitle: "book covers Aus der Ashche",
 					img: img2
 				},
 				{
-					title: "Aus der Asche",
-					subtitle: "Book by Wolfgang Bilinski",
-					img: img3
+					title: "logo",
+					subtitle: "Theresa company logo design",
+					img: img5
 				},
 				{
-					title: "Theresa Cosmetic Company",
-					subtitle: "Brochures",
+					title: "print",
+					subtitle: "brochures for Klöpfer lumber company",
 					img: img4
+				},
+				{
+					title: "print",
+					subtitle: "Michael Fitz CD booklet",
+					img: img6
+				},
+				{
+					title: "print",
+					subtitle: "Bay LGB design",
+					img: img7
 				}
 			]    
 		};
@@ -45,25 +57,19 @@ class ListProjects extends Component {
 		const projects = this.state.projects.map ( project => 
 				<Project project={project}/>
 			)
-
-		const styles = {
-				  root: {
-				    display: 'flex',
-				    flexWrap: 'wrap',
-				    justifyContent: 'space-around',
-				  },
-				  gridList: {
-				    width: 500,
-				    height: 450,
-				    overflowY: 'auto',
-				  },
-				};
 		return(
-			
-			<div className="container">
-				{projects}
+			<div className="content-container">
+				<div className="left-menu-column">
+					<li>all</li>
+					<li>print</li>
+					<li>digital</li>
+					<li>logo</li>
+					<li>others</li>
+				</div>
+				<div className="image-row container">
+					{projects}
+				</div>
 			</div>
-			
 		);
 	}
 }
