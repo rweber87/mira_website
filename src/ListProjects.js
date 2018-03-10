b441de0572fa11a674e033f8b3768ada88e79770
Rob Weber <rob.weber87@gmail.com>
2018-03-10 17:51:01 -0500

    added more projects

--- src/ListProjects.js
+++ src/ListProjects.js
@@ -39,6 +39,42 @@ import img36 from './images/joe_images/joe1.jpg';
 import img37 from './images/joe_images/joe2.jpg';
 import img38 from './images/joe_images/joe3.jpg';
 import img39 from './images/joe_images/joe4.jpg';
+import img40 from './images/WillowGareth_Landingpage.jpg';
+import img41 from './images/willow_gareth_images/wg1.jpg';
+import img42 from './images/willow_gareth_images/wg2.jpg';
+import img43 from './images/willow_gareth_images/wg3.jpg';
+import img44 from './images/willow_gareth_images/wg4.jpg';
+import img45 from './images/willow_gareth_images/wg5.jpg';
+import img46 from './images/SpeakUp_Landingpage.jpg';
+import img47 from './images/speakup_images/su1.jpg';
+import img48 from './images/speakup_images/su2.jpg';
+import img49 from './images/Karina_Landingpage.jpg';
+import img50 from './images/karina_images/karina1.jpg';
+import img51 from './images/karina_images/karina2.jpg';
+import img52 from './images/karina_images/karina3.jpg';
+import img53 from './images/karina_images/karina4.jpg';
+import img54 from './images/karina_images/karina5.jpg';
+import img55 from './images/karina_images/karina6.jpg';
+import img56 from './images/karina_images/karina7.jpg';
+import img57 from './images/karina_images/karina8.jpg';
+import img58 from './images/EasyFrank_Landingpage.jpg';
+import img59 from './images/easy_frank_images/ef1.jpg';
+import img60 from './images/easy_frank_images/ef2.jpg';
+import img61 from './images/Theresa_Landingpage.jpg';
+import img62 from './images/theresa_images/t1.jpg';
+import img63 from './images/theresa_images/t2.jpg';
+import img64 from './images/theresa_images/t3.jpg';
+import img65 from './images/Logos_Landingpage.jpg';
+import img66 from './images/logo_images/logo1.jpg';
+import img67 from './images/logo_images/logo2.jpg';
+import img68 from './images/logo_images/logo3.jpg';
+import img69 from './images/logo_images/logo4.jpg';
+import img70 from './images/logo_images/logo5.jpg';
+import img71 from './images/logo_images/logo6.jpg';
+import img72 from './images/logo_images/logo7.jpg';
+import img73 from './images/logo_images/logo8.jpg';
+import img74 from './images/logo_images/logo9.jpg';
+import img75 from './images/logo_images/logo10.jpg';
 
 class ListProjects extends Component {
 
@@ -88,6 +124,48 @@ class ListProjects extends Component {
 					modal_text: "for this project i actually learned how to build a deck. the client gave almost no instruction and i built this from scratch...pun intended",
 					img: img35,
 					modal: [img36, img37, img38, img39]
+				},
+				{
+					title: "print",
+					subtitle: "wedding invitations",
+					modal_text: "a beautiful wedding that was accompanied by these amazing inviations, name holders etc...",
+					img: img40,
+					modal: [img41, img42, img43, img44, img45]
+				},
+				{
+					title: "print",
+					subtitle: "language school promo material",
+					modal_text: "here is some swag i made for a language school around blah blah blah",
+					img: img46,
+					modal: [img47, img48]
+				},
+				{
+					title: "other",
+					subtitle: "designs for wall decals",
+					modal_text: "a good friend had a baby and asked me to design some wall decals for the baby's room. i think they came out quite neat",
+					img: img49,
+					modal: [img50, img51, img52, img53, img54, img55, img56, img57 ]
+				},
+				{
+					title: "other",
+					subtitle: "CD cover design",
+					modal_text: "when my friend francois called me to ask if I would be willing to help him do the cover design for his new CD I was very excited. I love listening to him when he talks about his music, he is so passionate, so on fire. he invited me for dinner, told me about the idea behind his new cd and what it means to him. It's about his journey. »RESTLESS« is the perfect name for this album, especially if you know francois ;-) once the name was there, the rest just fell into place.",
+					img: img58,
+					modal: [img59, img60]
+				},
+				{
+					title: "logo and print",
+					subtitle: "cosmetic company designs",
+					modal_text: "a beautiful wedding that was accompanied by these amazing inviations, name holders etc...",
+					img: img61,
+					modal: [img62, img63, img64 ]
+				},
+				{
+					title: "logos",
+					subtitle: "examples of logos",
+					modal_text: "a beautiful wedding that was accompanied by these amazing inviations, name holders etc...",
+					img: img65,
+					modal: [img66, img67, img68, img69, img70, img71, img72, img73, img74, img75 ]
 				},
 			]    
 		};
