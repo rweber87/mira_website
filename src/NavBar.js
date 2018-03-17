c59fbe39dbecaa0cfb982c2639108063a4faeb40
Rob Weber <rob.weber87@gmail.com>
2018-03-17 14:54:41 -0400

    update navbar by removing grid and adding css

--- src/NavBar.js
+++ src/NavBar.js
@@ -6,21 +6,15 @@ function NavBar(props) {
 
 	return(
 		<div className="nav">
-			<Grid container spacing={24}>
-				 <Grid item xs={12} sm={6}>
-				 	<h1 className="navbar-title">mira <b>taferner</b></h1>
-				 </Grid>
-				 <div className="logo-line"></div>
-				<Grid item xs={12} sm={2} className="app-nav-item">
-					<Link to="/portfolio" className="nav-item">what I do</Link>
-				</Grid>
-				<Grid item xs={12} sm={2} className="app-nav-item">
-					<Link to="/about" className="nav-item">who I am</Link>
-				</Grid>
-				<Grid item xs={12} sm={2} className="app-nav-item">
-					<Link to="/contact" className="nav-item">get in touch</Link>
-				</Grid>
-			</Grid>
+			<div className="left-nav">
+				<h1 className="navbar-title">mira <b>taferner</b></h1>
+				<div className="logo-line"></div>
+			</div>
+			<div className="right-nav">
+				<Link to="/portfolio" className="nav-item">what I do</Link>
+				<Link to="/about" className="nav-item">who I am</Link>
+				<Link to="/contact" className="nav-item">get in touch</Link>
+			</div>
 		</div>
 	)
 }
