f80babc7537ca38f3c64d23f174dd84dfbee35e9
Rob Weber <rob.weber87@gmail.com>
2018-03-17 21:28:01 -0400

    version before deploying

--- src/About.js
+++ src/About.js
@@ -6,18 +6,27 @@ function About() {
 	return (
 		<div className="content-container">
 			<div className="left-menu-column">
-				<li>Fun Facts</li>
+				<li>Fun Facts:</li>
 				<li>- love cats, not in a weird way</li>
 				<li>- fluent in bavarian and pig latin</li>
 				<li>- night owl</li>
-				<li>- did a tough mudder and hated<br/>&nbsp;&nbsp;the swimming part of it</li>
+				<li>- did a triathlon and didn't enjoy<br/>&nbsp;&nbsp;the swimming part of it</li>
+				<li>- love eating avocado</li>
+				<li>- 3 older siblings</li>
+				<li>- i love my boyfriend, Rob</li>
+				<li>- xxx</li>
 			</div>
 			<div className="right-menu-column" >
 				<div className="img-window">
 					<img id={"headshot"} src={headshot} />
 				</div>
-				<div className="about-text" > Lorem ipsum dolor sit amet consectetur adipiscing elit vehicula, torquent leo scelerisque aenean arcu quam orci per, libero nullam id nibh eget sollicitudin magnis. Potenti nascetur nibh a gravida viverra praesent tristique dapibus auctor posuere porttitor, semper nec fames ridiculus commodo ligula at consequat justo mi, inceptos risus odio accumsan erat vestibulum elementum pretium donec cursus. Sem velit in pretium per habitasse sociosqu feugiat fringilla lectus, sapien nulla tempus pharetra tempor semper neque felis.<br/><br/>
-				Himenaeos nec sapien curae dapibus conubia augue sociosqu, placerat torquent congue quam nam facilisis, faucibus suspendisse tempus per parturient duis. Vulputate a non mattis rhoncus sem mollis nulla natoque, platea sociosqu viverra scelerisque mus magna molestie, dictumst morbi est potenti feugiat taciti etiam. Phasellus euismod urna interdum a nostra class sagittis facilisis non, ante vulputate neque vel nam luctus placerat iaculis leo facilisi, curae conubia aliquam varius etiam dis quis sapien.
+				<div className="about-text" > My name is Mira and I am a Munich based graphic designer with 7 years of working experience with a diverse selection of clients and projects. After graduating from MDH in Munich in 2010, I found a small graphic design agency that I have been working for till today. For the last 3 years I have also been doing free-lance work on the side. I enjoy working as a team, to hear  different opinions and see other angles and approaches. I see opportunity to learn, grow, inspire and communicate with every job that comes my way. I appreciate honest conversation, strategic thinking, fun brainstorming, working closely with my clients throughout the design process and building client relationships that last.<br/><br/>
+				<strong>I want to create good looking stuff that works.</strong><br/><br/>
+				When I am not working, I passionately devote my time to Barbershop music and the community that comes with it.<br/>
+				I enjoy listening to live music, eating healthy food, dreaming about and creating a better future and spending time with people that inspire and uplift me.<br/><br/>
+				Favorite quote atm:<br/><br/>
+				"you can, you should, and if you’re brave enough to start, you will.""
+				- Stephen King
 				</div>
 				<div className="decorative-line"></div>
 				<div className="about-text">
--- src/Contact.js
+++ src/Contact.js
@@ -4,12 +4,19 @@ function Contact() {
 	return(
 		<div className="content-container">
 			<div className="left-menu-column social-icons">
-				<li><div className="social-media facebook"><a href={"https://www.facebook.com/mira.taferner"} className={"fa fa-facebook"}></a></div></li>
-				<li><div className="social-media linkedin"><a href={"https://www.linkedin.com/in/mira-taferner-5b54a563/"} className={"fa fa-linkedin"}></a></div></li>
-				<li><div className="social-media instagram"><a href={"https://www.instagram.com/schnago/"} className={"fa fa-instagram"}></a></div></li>
 			</div>
-			<div className="image-row container">
-				Here's some text!
+			<div className="right-menu-column container">
+				<strong>like my work?</strong><br/><br/>
+				Want to talk about your project? Then get in touch!<br/>
+				I am looking forward to hearing from you.<br/><br/><br/><br/>
+				<strong>Mira Taferner</strong><br/><br/>
+				<a className="email" href="mailto:mira.taferner@n0t.net">mira.taferner@n0t.net</a><br/><br/>
+				+49 152 540 78 139<br/><br/><br/><br/>
+				<div>or find me here &nbsp;&nbsp;<a className="social-media linkedin" href={"https://www.linkedin.com/in/mira-taferner-5b54a563/"} className={"fa fa-linkedin"}></a>&nbsp;&nbsp; or here &nbsp;&nbsp;<a className="social-media instagram" href={"https://www.instagram.com/schnago/"} className={"fa fa-instagram"}></a></div>
+				
+				{/*<li><div className="social-media facebook"><a href={"https://www.facebook.com/mira.taferner"} className={"fa fa-facebook"}></a></div></li>
+				<li><div className="social-media linkedin"><a href={"https://www.linkedin.com/in/mira-taferner-5b54a563/"} className={"fa fa-linkedin"}></a></div></li>
+				<li><div className="social-media instagram"><a href={"https://www.instagram.com/schnago/"} className={"fa fa-instagram"}></a></div></li>*/}
 			</div>
 		</div>
 	)
