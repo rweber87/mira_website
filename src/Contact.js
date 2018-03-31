45701d12fba88ccf19af9b61a7bb73a188f37271
Rob Weber <rob.weber87@gmail.com>
2018-03-31 18:48:08 -0400

    updated fb with behance logo and url

--- src/Contact.js
+++ src/Contact.js
@@ -11,7 +11,7 @@ function Contact() {
 				<div className="contact-name">Mira Taferner</div>
 				<div className="contact-email"><a className="email" href="mailto:mira.taferner@n0t.net">mira.taferner@n0t.net</a></div>
 				<div className="contact-phone-number">+49 152 540 78 139</div>
-				<div className="contact-social">or find me here &nbsp;&nbsp;<a className="social-media linkedin" href={"https://www.linkedin.com/in/mira-taferner-5b54a563/"} className={"fa fa-linkedin"}></a>&nbsp;&nbsp; or here &nbsp;&nbsp;<a className="social-media instagram" href={"https://www.instagram.com/schnago/"} className={"fa fa-instagram"}></a></div>
+				<div className="contact-social">or find me here &nbsp;&nbsp;<a className="social-media linkedin" href={"https://www.linkedin.com/in/mira-taferner-5b54a563/"} className={"fa fa-linkedin"}></a>&nbsp;&nbsp; or here &nbsp;&nbsp;<a className="social-media instagram" href={"https://www.behance.net/miratafernbf78"} className={"fa fa-behance"}></a></div>
 				
 				{/*<li><div className="social-media facebook"><a href={"https://www.facebook.com/mira.taferner"} className={"fa fa-facebook"}></a></div></li>
 				<li><div className="social-media linkedin"><a href={"https://www.linkedin.com/in/mira-taferner-5b54a563/"} className={"fa fa-linkedin"}></a></div></li>
